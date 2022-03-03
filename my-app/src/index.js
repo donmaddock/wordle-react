aec2458e96cbbb8b72de9f92175d3ed270ee191a
donmaddock <48396498+donmaddock@users.noreply.github.com>
2022-03-02 20:32:47 -1000

    basic play finished

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './style.css';
 import 'semantic-ui-css/semantic.min.css';
 import { Header, Menu, Container, Dropdown, Icon, Grid, GridRow, Button} from 'semantic-ui-react';
+import * as words from './word-bank.json';
+
+const wordBank = words;
+let keyWord = wordBank.valid[Math.floor(Math.random() * wordBank.valid.length)];
+keyWord = keyWord.toUpperCase();
+let guessedLetters = [];
 
 class TopMenu extends  React.Component {
 
@@ -42,7 +48,8 @@ class Board extends React.Component {
       word4: '',
       word5: '',
       word6: '',
-      currentWord: 'word1'
+      currentWord: 'word1',
+      currentMessage: 'I\'ll let you know what\'s up'
     };
   }
 
@@ -55,8 +62,7 @@ class Board extends React.Component {
   }
 
   handleClick = () => {
-    let key = this.state.currentWord;
-    console.log(this.state[key]);
+    console.log(setColors('AEIOU'));
   }
 
   handleKey = (e) => {
@@ -95,11 +101,18 @@ class Board extends React.Component {
       console.log("endgame");
       return;
     }
-    //check for valid word here
-    num++;
-    currentWord = "word" + num;
-    this.setState( {currentWord: currentWord});
-    this.setState({currentGuess: ''});
+
+    if(isValidWord(this.state.currentGuess)){
+      let key = this.state.currentWord;
+      this.setState({ [key] : this.state.currentGuess + setColors(this.state.currentGuess)});
+      num++;
+      currentWord = "word" + num;
+      this.setState( {currentWord: currentWord});
+      this.setState({currentGuess: ''});
+    }else{
+      this.setState({currentMessage: this.state.currentGuess + ' is not a valid word'});
+    }
+
   }
 
   deleteLetter = () => {
@@ -115,46 +128,46 @@ class Board extends React.Component {
     return (
       <Container>
         <div className='wordrow'>
-          <Tile tileType='empty' displayedLetter={this.state.word1[0]} />
-          <Tile tileType='empty' displayedLetter={this.state.word1[1]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word1[2]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word1[3]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word1[4]}/>
+          <Tile tileType={letterToClass(this.state.word1[5])} displayedLetter={this.state.word1[0]} />
+          <Tile tileType={letterToClass(this.state.word1[6])} displayedLetter={this.state.word1[1]}/>
+          <Tile tileType={letterToClass(this.state.word1[7])} displayedLetter={this.state.word1[2]}/>
+          <Tile tileType={letterToClass(this.state.word1[8])} displayedLetter={this.state.word1[3]}/>
+          <Tile tileType={letterToClass(this.state.word1[9])} displayedLetter={this.state.word1[4]}/>
         </div>
         <div className='wordrow'>
-          <Tile tileType='empty' displayedLetter={this.state.word2[0]} />
-          <Tile tileType='empty' displayedLetter={this.state.word2[1]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word2[2]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word2[3]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word2[4]}/>
+          <Tile tileType={letterToClass(this.state.word2[5])} displayedLetter={this.state.word2[0]} />
+          <Tile tileType={letterToClass(this.state.word2[6])} displayedLetter={this.state.word2[1]}/>
+          <Tile tileType={letterToClass(this.state.word2[7])} displayedLetter={this.state.word2[2]}/>
+          <Tile tileType={letterToClass(this.state.word2[8])} displayedLetter={this.state.word2[3]}/>
+          <Tile tileType={letterToClass(this.state.word2[9])} displayedLetter={this.state.word2[4]}/>
         </div>
         <div className='wordrow'>
-          <Tile tileType='empty' displayedLetter={this.state.word3[0]} />
-          <Tile tileType='empty' displayedLetter={this.state.word3[1]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word3[2]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word3[3]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word3[4]}/>
+          <Tile tileType={letterToClass(this.state.word3[5])} displayedLetter={this.state.word3[0]} />
+          <Tile tileType={letterToClass(this.state.word3[6])} displayedLetter={this.state.word3[1]}/>
+          <Tile tileType={letterToClass(this.state.word3[7])} displayedLetter={this.state.word3[2]}/>
+          <Tile tileType={letterToClass(this.state.word3[8])} displayedLetter={this.state.word3[3]}/>
+          <Tile tileType={letterToClass(this.state.word3[9])} displayedLetter={this.state.word3[4]}/>
         </div>
         <div className='wordrow'>
-          <Tile tileType='empty' displayedLetter={this.state.word4[0]} />
-          <Tile tileType='empty' displayedLetter={this.state.word4[1]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word4[2]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word4[3]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word4[4]}/>
+          <Tile tileType={letterToClass(this.state.word4[5])} displayedLetter={this.state.word4[0]} />
+          <Tile tileType={letterToClass(this.state.word4[6])} displayedLetter={this.state.word4[1]}/>
+          <Tile tileType={letterToClass(this.state.word4[7])} displayedLetter={this.state.word4[2]}/>
+          <Tile tileType={letterToClass(this.state.word4[8])} displayedLetter={this.state.word4[3]}/>
+          <Tile tileType={letterToClass(this.state.word4[9])} displayedLetter={this.state.word4[4]}/>
         </div>
         <div className='wordrow'>
-          <Tile tileType='empty' displayedLetter={this.state.word5[0]} />
-          <Tile tileType='empty' displayedLetter={this.state.word5[1]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word5[2]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word5[3]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word5[4]}/>
+          <Tile tileType={letterToClass(this.state.word5[5])} displayedLetter={this.state.word5[0]} />
+          <Tile tileType={letterToClass(this.state.word5[6])} displayedLetter={this.state.word5[1]}/>
+          <Tile tileType={letterToClass(this.state.word5[7])} displayedLetter={this.state.word5[2]}/>
+          <Tile tileType={letterToClass(this.state.word5[8])} displayedLetter={this.state.word5[3]}/>
+          <Tile tileType={letterToClass(this.state.word5[9])} displayedLetter={this.state.word5[4]}/>
         </div>
         <div className='wordrow'>
-          <Tile tileType='empty' displayedLetter={this.state.word6[0]} />
-          <Tile tileType='empty' displayedLetter={this.state.word6[1]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word6[2]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word6[3]}/>
-          <Tile tileType='empty' displayedLetter={this.state.word6[4]}/>
+          <Tile tileType={letterToClass(this.state.word6[5])} displayedLetter={this.state.word6[0]} />
+          <Tile tileType={letterToClass(this.state.word6[6])} displayedLetter={this.state.word6[1]}/>
+          <Tile tileType={letterToClass(this.state.word6[7])} displayedLetter={this.state.word6[2]}/>
+          <Tile tileType={letterToClass(this.state.word6[8])} displayedLetter={this.state.word6[3]}/>
+          <Tile tileType={letterToClass(this.state.word6[9])} displayedLetter={this.state.word6[4]}/>
         </div>
 
         <Button onClick={this.handleClick}>
@@ -162,6 +175,8 @@ class Board extends React.Component {
         </Button>
 
         <h1>Your guess for {this.state.currentWord} is {this.state.currentGuess}</h1>
+        <h1>The Keyword is {keyWord}</h1>
+        <h1>{this.state.currentMessage}</h1>
 
       </Container>
     )
@@ -176,9 +191,48 @@ function Tile(props){
   );
 }
 
-function Word(props){
-
+function isValidWord(word){
+  return (wordBank.valid.includes(word.toLowerCase()) || wordBank.invalid.includes(word.toLowerCase()));
 }
+
+function setColors(word){
+  let keyWordArray = [];
+  let colors = '';
+  for(let i = 0; i < 5; i++){
+    keyWordArray.push(keyWord[i]);
+  }
+  for(let i = 0; i < 5; i++){
+    let color = 'W';
+    if(keyWordArray.includes(word[i])){
+      color = 'Y';
+    }
+
+    if(word[i] == keyWordArray[i]){
+      color = 'G';
+    }
+
+    colors += color;
+  }
+  return colors;
+}
+
+function letterToClass(l){
+  switch (l){
+    case undefined:
+      return 'empty';
+
+    case 'W':
+      return  'wrong';
+
+    case 'Y':
+      return 'almost';
+
+    case 'G':
+      return 'correct';
+  }
+}
+
+
 
 class Wordle extends React.Component {
 
